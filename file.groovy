402d6002c9910539f89316190ff0f8215bea78aa
sumitk-Cloud <sumitkamble5550@gamil.com>
2026-09-21 08:54:39 -0700

    added

---
+++ file.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                git branch: 'main', url: 'https://github.com/iamtruptimane/ChatBot-Deployment.git'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
